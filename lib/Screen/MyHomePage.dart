30916a30ba67c3d1b796dcb2385a7c8366bc90e0
islambou9427 <islem.bouagada@gmail.com>
2020-12-05 13:33:36 +0100

    finish Custom app Bar

--- lib/Screen/MyHomePage.dart
+++ lib/Screen/MyHomePage.dart
@@ -1,3 +1,4 @@
+import 'package:classroom/componants/appbar.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -8,13 +9,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return SingleChildScrollView(
-      child: Scaffold(
-        body: Container(
-          child: Image.asset(
-            "bg.png",
-            fit: BoxFit.cover,
-          ),
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("bg.png"),
+          fit: BoxFit.cover,
+        )),
+        child: Column(
+          children: [
+            CustomAppBar(),
+          ],
         ),
       ),
     );
---
+++ lib/componants/appbar.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(30),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, -21),
+              blurRadius: 30,
+              color: Colors.black.withOpacity(0.16),
+            )
+          ]),
+      child: Row(
+        children: [
+          Image.asset(
+            "logo.png",
+            alignment: Alignment.topCenter,
+          ),
+          Spacer(),
+          MenuItem(
+            title: "About",
+            onPress: () {
+              print("About pressed");
+            },
+          ),
+          MenuItem(
+            title: "Courses",
+            onPress: () {
+              print("Courses pressed");
+            },
+          ),
+          MenuItem(
+            title: "Instructor",
+            onPress: () {
+              print("Instructor pressed");
+            },
+          ),
+          MenuItem(
+            title: "News",
+            onPress: () {
+              print("News pressed");
+            },
+          ),
+          MenuItem(
+            title: "Pricing",
+            onPress: () {
+              print("Pricing pressed");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  final String title;
+  final Function onPress;
+
+  const MenuItem({Key key, this.title, this.onPress}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: onPress,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.black38,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              decoration: TextDecoration.none,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
