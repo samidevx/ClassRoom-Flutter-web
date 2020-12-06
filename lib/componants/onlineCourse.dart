7044e2f4e233db9669c4e49e4a3a8fe39e6f799f
islambou9427 <islem.bouagada@gmail.com>
2020-12-06 21:56:12 +0100

    top container  of Online COurses without video Player

--- lib/componants/onlineCourse.dart
+++ lib/componants/onlineCourse.dart
@@ -8,7 +8,48 @@ class OnlineCourse extends StatelessWidget {
     return Container(
       width: width,
       height: height,
-      color: Colors.black38,
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 100),
+          Text(
+            "Online courses from 140 Top\nInstitutions.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color.fromRGBO(59, 41, 84, 1),
+              fontWeight: FontWeight.normal,
+              fontSize: 35,
+              decoration: TextDecoration.none,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            "Choose from 100,000 online video courses with new\nadditions published every mounth",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color.fromRGBO(59, 41, 84, 1),
+              fontWeight: FontWeight.normal,
+              fontSize: 13,
+              decoration: TextDecoration.none,
+            ),
+          ),
+          SizedBox(height: 20),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: FlatButton(
+              height: 50,
+              onPressed: () {},
+              child: Text("   Start courses  ",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 13)),
+              color: Color.fromRGBO(59, 41, 84, 1),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
