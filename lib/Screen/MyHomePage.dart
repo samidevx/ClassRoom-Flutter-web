892cb2fb87d482eb22da2492ac308eb08f346b30
islambou9427 <islem.bouagada@gmail.com>
2020-12-07 15:39:59 +0100

    start bottom online cource

--- lib/Screen/MyHomePage.dart
+++ lib/Screen/MyHomePage.dart
@@ -1,4 +1,5 @@
 import 'package:classroom/componants/Facepage.dart';
+import 'package:classroom/componants/bottomonlinecourse.dart';
 import 'package:classroom/componants/onlineCourse.dart';
 
 import 'package:flutter/material.dart';
@@ -12,12 +13,13 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     //double width = MediaQuery.of(context).size.width;
-   // double height = MediaQuery.of(context).size.height;
+    // double height = MediaQuery.of(context).size.height;
     return SingleChildScrollView(
       child: Column(
         children: [
           FacePage(),
           OnlineCourse(),
+          BottomOnlineCourse(),
         ],
       ),
     );
---
+++ lib/componants/bottomonlinecourse.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BottomOnlineCourse extends StatefulWidget {
+  @override
+  _BottomOnlineCourseState createState() => _BottomOnlineCourseState();
+}
+
+class _BottomOnlineCourseState extends State<BottomOnlineCourse> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
