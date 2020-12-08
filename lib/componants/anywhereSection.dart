8286eadca5697b2dda33c7e525964fd1f835f5d7
islambou9427 <islem.bouagada@gmail.com>
2020-12-08 14:24:02 +0100

    anywhere section done !

--- lib/componants/anywhereSection.dart
+++ lib/componants/anywhereSection.dart
@@ -14,6 +14,69 @@ class _AnyWhereSectionState extends State<AnyWhereSection> {
       width: width,
       height: height,
       color: Color.fromRGBO(245, 237, 254, 1),
+      child: Row(
+        children: [
+          Container(width: width * 0.50, child: Image.asset("hours.png")),
+          Container(
+            padding: EdgeInsets.all(70),
+            width: width * 0.50,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: height * 0.17,
+                ),
+                Text(
+                  "Learn From\nanywhere.",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Color.fromRGBO(59, 41, 84, 1),
+                    fontWeight: FontWeight.normal,
+                    fontSize: 55,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                SizedBox(
+                  height: height * 0.05,
+                ),
+                Text(
+                  "Take classes on the go with the Skillshare app.\nStream or download to watch on the plane, the\nsubway,or wherever you learn best.",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Color.fromRGBO(59, 41, 84, 1),
+                    fontWeight: FontWeight.normal,
+                    fontSize: 18,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                SizedBox(
+                  height: height * 0.10,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: FlatButton(
+                    height: 47,
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                          color: Color.fromRGBO(59, 41, 84, 1),
+                          width: 0.5,
+                          style: BorderStyle.solid),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text("     Download    ",
+                        style: TextStyle(
+                            color: Color.fromRGBO(59, 41, 84, 1),
+                            fontWeight: FontWeight.w300,
+                            fontSize: 13)),
+                    color: Color.fromRGBO(245, 237, 254, 1),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
