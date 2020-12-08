23e5fe87caf3b27ffae3b70539d8277e236b62db
islambou9427 <islem.bouagada@gmail.com>
2020-12-08 15:04:15 +0100

    startCoursesSection Donne !

--- lib/componants/startCourses.dart
+++ lib/componants/startCourses.dart
@@ -19,17 +19,40 @@ class _StartingCoursesState extends State<StartingCourses> {
         child: Column(
           children: [
             Container(
-              color: Colors.pink,
               height: height * 0.33,
+              child: Image.asset(
+                "ph1.png",
+                width: width,
+              ),
             ),
             Expanded(
               child: Row(
-                children: [],
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    width: width * 0.40,
+                    child: Text(
+                      "Courses\nstarting soon!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color.fromRGBO(59, 41, 84, 1),
+                        fontWeight: FontWeight.normal,
+                        fontSize: 55,
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                  ),
+                  Container(width: width * 0.40, child: Image.asset("ph3.png")),
+                ],
               ),
             ),
             Container(
-              color: Colors.blue,
               height: height * 0.33,
+              child: Image.asset(
+                "ph2.png",
+                width: width,
+              ),
             ),
           ],
         ),
