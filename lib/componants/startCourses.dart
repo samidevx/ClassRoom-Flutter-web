79681cc6eee131ed6a495f40bfb9ddca1ffee448
islambou9427 <islem.bouagada@gmail.com>
2020-12-08 14:44:05 +0100

    define startCourses Structure

--- lib/componants/startCourses.dart
+++ lib/componants/startCourses.dart
@@ -8,6 +8,32 @@ class StartingCourses extends StatefulWidget {
 class _StartingCoursesState extends State<StartingCourses> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Padding(
+      padding: const EdgeInsets.all(100.0),
+      child: Container(
+        width: width,
+        height: height,
+        color: Colors.white,
+        child: Column(
+          children: [
+            Container(
+              color: Colors.pink,
+              height: height * 0.33,
+            ),
+            Expanded(
+              child: Row(
+                children: [],
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              height: height * 0.33,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
