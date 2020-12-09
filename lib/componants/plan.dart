ef25afa91a5cbf8932c45fab57d4074e9371e0ba
islambou9427 <islem.bouagada@gmail.com>
2020-12-09 19:04:23 +0100

    plan section structure

--- lib/componants/plan.dart
+++ lib/componants/plan.dart
@@ -3,6 +3,46 @@ import 'package:flutter/material.dart';
 class PlanForPay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        width: width,
+        height: height,
+        color: Color.fromRGBO(59, 41, 84, 1),
+        child: Column(
+          children: [
+            Container(
+              color: Colors.pink,
+              height: height * 0.35,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Image.asset(""),
+                      Text(""),
+                    ],
+                  ),
+                  Text(""),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.black,
+              height: height * 0.35,
+              child: Row(
+                children: [
+                  CardItem(),
+                  CardItem(),
+                ],
+              ),
+              
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
