a33fa35e3032cf575efce15149e9d7d8b151736f
islambou9427 <islem.bouagada@gmail.com>
2020-12-09 21:09:19 +0100

    PLan Section done !

---
+++ lib/Screen/platitem.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class PlanItem extends StatelessWidget {
+  const PlanItem({this.title, this.prix, this.title2});
+
+  final String title;
+  final String prix;
+
+  final String title2;
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Container(
+      width: width / 5.2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(width: 1, color: Colors.white),
+      ),
+      height: height * 0.5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 23,
+              decoration: TextDecoration.none,
+            ),
+          ),
+          Text(
+            prix,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 23,
+              decoration: TextDecoration.none,
+            ),
+          ),
+          Text(
+            "Per Month / Paid Annually",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.normal,
+              fontSize: 13,
+              decoration: TextDecoration.none,
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: FlatButton(
+              height: 47,
+              onPressed: () {},
+              child: Text("     Select Plan    ",
+                  style: TextStyle(
+                      color: Color.fromRGBO(59, 41, 84, 1),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 13)),
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            title2,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.normal,
+              fontSize: 13,
+              decoration: TextDecoration.none,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/componants/plan.dart
+++ lib/componants/plan.dart
@@ -1,3 +1,4 @@
+import 'package:classroom/Screen/platitem.dart';
 import 'package:flutter/material.dart';
 
 class PlanForPay extends StatelessWidget {
@@ -5,66 +6,76 @@ class PlanForPay extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Container(
-        width: width,
-        height: height,
-        color: Color.fromRGBO(47, 45, 81, 1),
-        child: Column(
-          children: [
-            Container(
-              height: height * 0.35,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset("lo.png"),
-                  SizedBox(
-                    width: 40,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Welcome to classroom!",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 33,
-                          decoration: TextDecoration.none,
-                        ),
+    return Container(
+      width: width,
+      height: height,
+      color: Color.fromRGBO(47, 45, 81, 1),
+      child: Column(
+        children: [
+          Container(
+            height: height * 0.35,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("lo.png"),
+                SizedBox(
+                  width: 40,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Welcome to classroom!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 33,
+                        decoration: TextDecoration.none,
                       ),
-                      SizedBox(
-                        height: 25,
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Text(
+                      "Get full access to thousands of classes or begin\nClassroom Basic",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 13,
+                        decoration: TextDecoration.none,
                       ),
-                      Text(
-                        "Get full access to thousands of classes or begin\nClassroom Basic",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.normal,
-                          fontSize: 13,
-                          decoration: TextDecoration.none,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Container(
-              color: Colors.blue,
-              height: height * 0.65,
-              child: Row(
-                children: [
-                  // CardItem(),
-                  //CardItem(),
-                ],
-              ),
+          ),
+          Container(
+            height: height * 0.65,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                PlanItem(
+                  title: "Basic",
+                  prix: "\$99",
+                  title2:
+                      "*60 minute classes 3 weeks expiration\ndate on all class packages",
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                PlanItem(
+                  title: "Pro",
+                  prix: "\$190",
+                  title2:
+                      "*90 minute classes 5 weeks expiration\ndate on all class packages",
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
